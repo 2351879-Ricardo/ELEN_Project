e4188388ada8f141694c4573489d260856841110
Joshua-Jandrell <2333213@students.wits.ac.za>
2022-05-26 13:15:13 +0200

    Testsed and updated energyCalc

--- CODE/calculation/unitConverter.py
+++ CODE/calculation/unitConverter.py
@@ -1,3 +1,4 @@
+# version 0.1
 # Power.log by GROOP - 2022
 # Programed by Joshua Jnadrell 
 
--- CODE/test_energyCalc.py
+++ CODE/test_energyCalc.py
@@ -1,5 +1,5 @@
 import unittest
-import calculation.energyCalc
+import calculation.energyCalc as energyCalc
 
 class testEnergyCalc(unittest.TestCase):
     def test_distanceTravelled_1(self):
@@ -14,5 +14,18 @@ class testEnergyCalc(unittest.TestCase):
         self.assertAlmostEqual(energyCalc.fuelUsed(142, 8.2), 11.644)
     def test_fuelUsed_2(self):
         self.assertAlmostEqual(energyCalc.fuelUsed(301, 6.3), 18.963)
+
+# Added to thes version 0.2
+# Unlike the first set of tests - thes tests are dependant on the unitconverter moudle
+class testEnergyCalc_2(unittest.TestCase):
+    def test_fuelEnergy_petrol_1(self):
+        self.assertAlmostEqual(energyCalc.fuelEnergy("petrol", 32.1), 304.95)
+    def test_fuelEnergy_diesel_1(self):
+        self.assertAlmostEqual(energyCalc.fuelEnergy("diesel", 32.1), 344.112)
+
+    def test_energyUsed_1(self):
+        self.assertAlmostEqual(energyCalc.energyUsed(1012.6, 1000.1, 6.2, "petrol"), 7.3625)
+    def test_energyUsed_2(self):
+        self.assertAlmostEqual(energyCalc.energyUsed(3421, 3386.2, 7.82, "diesel"), 29.1729792)
 if __name__ == '__main__':
     unittest.main()
