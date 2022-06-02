0c685f17a9d5c2d69bdbf3b6b498071918847daa
Andrew Kapp <2329108@students.wits.ac.za>
2022-06-02 15:45:47 +0200

    Python files, ready for some tests

--- CODE/FILE_POPULATION/filePopulation.py
+++ CODE/FILE_POPULATION/filePopulation.py
@@ -1,2 +1,14 @@
-# PYTHON SCRIPT
+
+#Takes in the name of the file (without the type) as a string, and a log as a string.
+#Prints the log string to the file
+def dataIn(fileName, log):
+	with open(fileName+'.txt', 'w') as f:
+		print(log, file=f)
+
+#takes in the file name (without file type) 
+#returns all logs present in said file		
+def dataOut(fileName):
+	file1=open(fileName+'.txt', 'w')
+	logs=file1.readlines()
+	return logs
 
