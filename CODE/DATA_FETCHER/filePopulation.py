964a674efd9ff2843722653440720a5a14fa07c6
Khaya Ngwenya <2366883@students.wits.ac.za>
2022-06-06 19:48:11 +0200

    Incomplete Data Fetcher

---
+++ CODE/DATA_FETCHER/filePopulation.py
@@ -0,0 +1,14 @@
+
+#Takes in the name of the file (without the type) as a string, and a log as a string.
+#Prints the log string to the file
+def dataIn(fileName, log):
+	with open(fileName+'.txt', 'a') as f:
+		print(log, file=f)
+
+#takes in the file name (without file type) 
+#returns all logs present in the chosen file		
+def dataOut(fileName):
+	file1=open(fileName+'.txt', 'r+')
+	logs=file1.readlines()
+	return logs
+
