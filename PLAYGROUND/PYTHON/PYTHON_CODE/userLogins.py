fb74453c0b86b001e748499b59da40f39c9c6f56
2351879-Ricardo <2351879@students.wits.ac.za>
2022-06-07 18:30:26 +0200

    Finished Encryption

--- PLAYGROUND/PYTHON/PYTHON_CODE/userLogins.py
+++ PLAYGROUND/PYTHON/PYTHON_CODE/userLogins.py
@@ -3,37 +3,49 @@ import os
 import filePopulation
 
 def UserExists(userID):
-    lines = filePopulation.dataOut('users')
-    userExists = False
-    for line in lines:
-        splitLine = line.split("-")
-        if (splitLine[0] == userID):
-            userExists = True
-            break;
-    return userExists
+    lines = filePopulation.byteOut('users')
+    i = 0
+    while (i < len(lines)):
+        if (i == 0):
+            i += 1
+        if (i % 2 == 1):
+            userName = lines[i].decode().replace('\n', '')
+            if (userName == userID):
+                return True
+            i += 1
+
+        else:
+            i += 1
+
+    return False
 
 def GetUserEncrypt(userID):
-    lines = filePopulation.dataOut('users')
-    for thisLine in lines:
-        print("Line Read")
-        print(thisLine)
-        splitLine = thisLine.split("-")
-        if (splitLine[0] == userID):
-            return splitLine[1]
+    lines = filePopulation.byteOut('users')
+    i = 0
+    while (i < len(lines)):
+        if (i == 0):
+            i += 1
+        if (i % 2 == 1):
+            userName = lines[i].decode().replace('\n', '')
+            if (userName == userID):
+                return lines[i+1]
+            i += 1
 
-    return "NO INFO"
+        else:
+            i += 1
+
 
 def GenerateKey(password):
     salt = os.urandom(32)
     key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
     storePrivate = salt + key
-    print("Store Private")
-    print(storePrivate)
     return storePrivate
 
 def WriteNewUser(userID, storePassword):
-    userInfo = str(userID) + "-" + str(storePassword)
-    filePopulation.dataIn('users', userInfo)    
+    filePopulation.byteIn('users', b'\n')
+    filePopulation.byteIn('users', userID.encode())
+    filePopulation.byteIn('users', b'\n')
+    filePopulation.byteIn('users', storePassword)
 
 
 def CreateUser(userID, password):
@@ -41,21 +53,13 @@ def CreateUser(userID, password):
     WriteNewUser(userID, storePassword)
 
 def IsValidSignIn(userID, password):
-    print("tester")
+    
     if (UserExists(userID)):
         encrypt = GetUserEncrypt(userID)
-        print("Encrypt")
-        print(encrypt)
-        # Change encrypt to bytes
         storageSalt = encrypt[:32]
-        print(storageSalt)
         storageKey = encrypt[32:]
         newKey = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), storageSalt, 100000)
-
         if (newKey == storageKey):
             return True
         else:
             return False
-
-CreateUser("Test", "Test")
-print(IsValidSignIn("Test", "Test"))
